708ea28ce23ff31aa0c214cc1d0adf6d89d9c7ac
Ya-Rong, Teng <yarong.teng@gmail.com>
2023-10-08 18:16:50 +0800

    chore: setup rspec and factory_bot

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,9 @@ module ChatApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |generate|
+      generate.fixture_replacement :factory_bot, dir: 'spec/factories/'
+    end
   end
 end
