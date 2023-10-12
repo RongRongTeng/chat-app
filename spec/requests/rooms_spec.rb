ca4426e126a7e2bc02ee3ea91f558c1c9c153e32
Ya-Rong, Teng <yarong.teng@gmail.com>
2023-10-12 22:07:07 +0800

    test(spec): add request test for GET /rooms/:id

--- spec/requests/rooms_spec.rb
+++ spec/requests/rooms_spec.rb
@@ -3,17 +3,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Rooms' do
-  let!(:user)  { create(:user) }
+  describe 'GET /rooms/:id' do
+    subject { get "/rooms/#{room.id}" }
 
-  before { sign_in user }
+    let!(:user) { create(:user) }
+    let!(:room) { create(:room) }
 
-  describe 'GET /rooms' do
-    subject { get '/rooms' }
+    before do
+      sign_in user
 
-    it 'returns http success' do
+      11.times do |n|
+        travel_to(n.minutes.ago) do
+          create(:room_message, receiveable: room, content: "message sent #{n} minutes ago in current room")
+        end
+      end
+
+      create(:room_message, content: 'Lastest message sent in other room')
+      create(:direct_message, receiveable: user, content: 'Lastest direct message')
+    end
+
+    it 'returns http success and render correct messages with paganation' do
       subject
 
       expect(response).to have_http_status(:success)
+
+      assert_select '.message-content', text: /message sent \d{1} minutes ago in current room/
+    end
+
+    it 'marks unread messages as read' do
+      expect { subject }.to change { user.notifications.unread.count }.from(13).to(2)
     end
   end
 end
